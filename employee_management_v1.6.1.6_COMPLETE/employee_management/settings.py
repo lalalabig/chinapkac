8d99e7f927d28b7c150c0b48af519290099d8235
lalalabig <anthony1132793245@outlook.com>
2025-11-02 23:41:15 +0200

    海外人员管理软件

--- employee_management_v1.6.1.6_COMPLETE/employee_management/settings.py
+++ employee_management_v1.6.1.6_COMPLETE/employee_management/settings.py
@@ -79,9 +79,7 @@ WSGI_APPLICATION = 'employee_management.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
